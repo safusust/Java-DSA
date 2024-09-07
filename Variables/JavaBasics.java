acbf36424e7de5fa31160a4059eb3b53b0615fd5
safusust <safusust@gmail.com>
2024-09-07 22:08:10 +0600

    First_Coomit

---
+++ Variables/JavaBasics.java
@@ -0,0 +1,10 @@
+package Variables;
+
+public class JavaBasics {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
+// print -> dont create new line 
+//println -> create new line 
+// /n -> create new line
---
+++ Variables/Variable.java
@@ -0,0 +1,14 @@
+package Variables;
+
+public class Variable {
+    
+}
+/*
+ * 2*(a+b)
+ * 2-> literal 
+ * a & b-> variable
+ */
+/*
+ * a,b, c
+ * identifier
+ */
